cb9f05c66071b05424fcd6caeb059838e05e4b2e
Seán Kenneally <Ctw20.sean.kenneally@corketb.ie>
2024-09-03 13:46:10 +0100

    Add files via upload

---
+++ 01 - Workbook - Getting Started.py
@@ -0,0 +1,18 @@
+#Sean Kenneally
+#Date 2/9/24
+#01 - Workbook - Getting Started
+
+print("Hello World")
+
+print("Hello, my names is Sean!")
+
+print("Hello World")
+
+print("Hello World")
+
+print("What is your age?")
+print(16)
+print("My age is 16")
+print(My age is", 16)
+print ("I am", 16, "My friend is", 17)
+print("The print command", "can handle", "more than 1 string.")
---
+++ intro.py
@@ -0,0 +1,13 @@
+#Author: Sean Kenneally
+#Date 2/9/24
+#Intro to Python - hello
+print("hello world!!!!")
+print("my name is Sean!!!!!")
+print(":) :) :)")
+print("aaaaaaaa")
+
+Name = "sean kenneally"
+print(Name)
+
+e = "Spider-Man"
+print(e)
